c242d69e5b808a15a379ac7505c2990950bad8f6
Shubhi <singhal.shubhi.2002@gmail.com>
2026-09-11 23:19:34 +0530

    Fix production session cookies

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -23,7 +23,7 @@ load_dotenv(BASE_DIR / ".env")
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = 'django-insecure-b1x4fcg#+l#ha6%@3&hh5xh+2vy!_ytmsyotx#g1amop2k^-=_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -129,7 +129,7 @@ if not DEBUG:
 
     CSRF_COOKIE_SAMESITE = "None"
     CSRF_COOKIE_SECURE = True
-
+    
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
